3cc81db4df0ab31440766fd2ca3828ed3871be19
Ellio <ilya.turban@gmail.com>
2022-11-25 16:09:37 +0100

    New BOT ARRIVED!

---
+++ bot.py
@@ -0,0 +1,27 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+def greet_user(update,context):
+    print('/start')
+    update.message.reply_text("Don't do it")
+
+def talk_to_me(update,context):
+    text=update.message.text
+    print(text)
+    update.message.reply_text(text)
+def main():
+    mybot=Updater(settings.API_KEY, use_context=True)
+
+    dp=mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    logging.info('bot started')
+    mybot.start_polling()
+
+    mybot.idle()
+
+if __name__=='__main__':
+    main()
+
+
